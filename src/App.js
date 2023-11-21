447707952ac5e0a510498965b39aa173ed774ccd
mmmiz <miduho4soft@yahoo.co.jp>
2023-11-21 09:30:04 -0600

    added

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://test-server-api-a86y.onrender.com/api/message');
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>React Frontend</h1>
+      <p>Message from server: {message}</p>
+    </div>
+  );
+}
+
+export default App;
