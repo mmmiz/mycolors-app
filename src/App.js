70b6fe0be487efa9c80b20a443424a11b78248a6
mmmiz <miduho4soft@yahoo.co.jp>
2023-11-21 10:03:11 -0600

    added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,53 @@ import axios from 'axios';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [inputText, setInputText] = useState('');
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get('https://test-server-be.onrender.com/api/message');
-        setMessage(response.data.message);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://test-server-be.onrender.com/api/message');
+      setMessage(response.data.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Send a POST request to update the message on the server
+      await axios.post('https://test-server-be.onrender.com/api/message', {
+        message: inputText,
+      });
+
+      // Fetch the updated message
+      fetchData();
+    } catch (error) {
+      console.error('Error updating message:', error);
+    }
+  };
 
   return (
     <div>
       <h1>React Frontend</h1>
       <p>Message from server: {message}</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter something:
+          <input type="text" value={inputText} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
