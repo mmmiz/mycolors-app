3859e75691ffb2e88b1c6417e2751e1932361bf3
mmmiz <miduho4soft@yahoo.co.jp>
2023-11-21 20:36:25 -0600

    server

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import Test from '../page/Test';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import HomePage from './page/HomePage';
+import LoginPage from './page/LoginPage';
+import Test from './page/Test';
 
 export default function Routes() {
-  return useRoutes ([
-    {
-      path: "/test", element: <Test />
-    }
-  ])
+  const routeElements = useRoutes(
+    [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path: '/login',
+        element: <LoginPage />
+      },
+      {
+        path: '/test',
+        element: <Test />
+      }
+    ],
+    { basename: '/unko' } // Set basename here
+  );
+
+  return (
+    <div>
+      <header>
+        {routeElements}
+      </header>
+    </div>
+  );
 }
+
