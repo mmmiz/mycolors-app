64c2216f3cafb0a1efd2891620abdc3126f9c12c
mmmiz <miduho4soft@yahoo.co.jp>
2023-11-23 02:27:24 -0600

    updated

--- src/pages/Allcolor/ColorDetail.js
+++ src/pages/Allcolor/ColorDetail.js
@@ -32,7 +32,7 @@ const apiUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:3000";
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
         // Details
-        axios.get(`https://mycolors-api-be.onrender.com/${orderNumber}`)
+        axios.get(`https://mycolors-api-be.onrender.com//all/getColors/${orderNumber}`)
           .then((response) => {
             setColorDetail(response.data);
             setMainPageBackground(response.data.mainColor.url);
