3bf7ac245c1f43b49ceda4dfbac099bbe47025d8
mmmiz <miduho4soft@yahoo.co.jp>
2023-12-08 11:33:09 -0600

    login updated

--- src/pages/Login/GuestLogin.js
+++ src/pages/Login/GuestLogin.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 const apiUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:3000";
 
-
 export default function GuestLogin() {
   const navigate = useNavigate();
 
@@ -12,27 +11,29 @@ export default function GuestLogin() {
     console.log("API URL:", apiUrl);
 
     try {
-      const response = await axios.post(`${apiUrl}/auth/login`,{
+      const response = await axios.post(`${apiUrl}/auth/login`, {
         email: 'guest_login@gmail.com',
         password: '12345678'
       });
       console.log(response.data.message);
-          const { token } = response.data;
-    localStorage.setItem('token', token);
+      const { token } = response.data;
+      localStorage.setItem('token', token);
       navigate('/?guestLoginSuccess=true');
-    } catch(error) {
+    } catch (error) {
       console.error('ERROR BOOM!', error);
     }
-  }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     }
-}, []);
+
+    return () => {};
+  }, []); 
 
   return (
     <p onClick={handleGuestLogin}>Guest Login</p>
-  )
+  );
 }
